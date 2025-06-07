a377b11937e490c8ba3d95b94dd91d0c3832ebcd
jiucai233 <jiucai@yonsei.ac.kr>
2025-06-07 17:36:15 +0900

    added web analytic function

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
+            <Analytics />
             <Footer />
 
             <Toaster position="top-right" />
